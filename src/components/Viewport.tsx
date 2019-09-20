46e6026ebdae3130814018cb7a300cb9f21e949e
Timothée Astier <timothee.astier@essec.edu>
2019-09-21 00:39:16 +0200

    feat: Add viewport

---
+++ src/components/Viewport.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { VIEWPORT_WIDTH_IN_PIXELS } from '../constants/config';
+
+const Wrapper = styled.div`
+  overflow: hidden;
+  width: ${() => `${VIEWPORT_WIDTH_IN_PIXELS}px`};
+  height: ${() => `${VIEWPORT_WIDTH_IN_PIXELS}px`};
+  background-color: black;
+  border: solid 5px black;
+  border-spacing: border-box;
+`;
+
+const Viewport: React.FC = (props) => {
+  return <Wrapper>{props.children}</Wrapper>;
+};
+
+export default Viewport;
--- src/constants/grid.ts
+++
@@ -1,4 +0,0 @@
-export const GRID_WIDTH = 20;
-export const GRID_HEIGHT = 20;
-
-export const CELL_WIDTH_IN_PIXELS = 30;
