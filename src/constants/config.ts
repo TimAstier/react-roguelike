a296fdc66845d5bf3705938a9fce7893f21b2d0c
Timothée Astier <timothee.astier@essec.edu>
2019-09-21 21:21:19 +0200

    improvement: Increase ratio of game view

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -2,7 +2,7 @@ export const ANIMATION_SPEED = 250; // ms
 export const ANIMATION_STEPS = 64;
 export const PAUSE_TIME_BETWEEN_MOVES = 100; // ms
 
-export const CELL_WIDTH_IN_PIXELS = 30;
+export const CELL_WIDTH_IN_PIXELS = 45;
 export const VIEWPORT_WIDTH_IN_PIXELS = 11 * CELL_WIDTH_IN_PIXELS + 2;
 
 export const GRID_WIDTH = 20; // number of cells
