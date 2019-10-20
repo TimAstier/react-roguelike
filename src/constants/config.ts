febe4062befec38e39460d18d01e9277b3cef75f
Timothée Astier <timothee.astier@essec.edu>
2019-10-21 00:40:00 +0200

    improvement: Adjust viewport width

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -3,7 +3,7 @@ export const ANIMATION_STEPS = 64;
 export const PAUSE_TIME_BETWEEN_MOVES = 100; // ms
 
 export const CELL_WIDTH_IN_PIXELS = 45;
-export const VIEWPORT_WIDTH_IN_PIXELS = 11 * CELL_WIDTH_IN_PIXELS + 2;
+export const VIEWPORT_WIDTH_IN_PIXELS = 11 * CELL_WIDTH_IN_PIXELS + 1;
 
 export const GRID_WIDTH = 20; // number of cells
 export const GRID_HEIGHT = 20; // number of cells
