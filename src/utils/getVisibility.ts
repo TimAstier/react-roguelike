2604cba3bc2bffb62340d2de3356b384271c8ae1
TimAstier <timothee.astier@storytel.com>
2021-04-23 10:30:02 +0200

    Add comments about how to add LOS

--- src/utils/getVisibility.ts
+++ src/utils/getVisibility.ts
@@ -10,6 +10,12 @@ interface Options {
 export const getVisibility = ({ posX, posY, playerPosition }: Options): Visibility => {
   const [playerX, playerY] = playerPosition;
 
+  // For all points potentially visible:
+  //   - draw a "supercover line" between the point and the player
+  //   - check if there is a wall in this line
+  //   - if so, the point is not visible by the player
+  // See: https://www.redblobgames.com/grids/line-drawing.html#supercover
+
   if (Math.abs(playerX - posX) === 2 && Math.abs(playerY - posY) === 2) {
     return 'dim';
   }
