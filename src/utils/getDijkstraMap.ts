562ad98e44fa104860fefc7aab3b9b29aca9bf05
TimAstier <timothee.astier@storytel.com>
2021-05-15 14:21:49 +0200

    Add performance benchmark to getDijkstraMap

--- src/utils/getDijkstraMap.ts
+++ src/utils/getDijkstraMap.ts
@@ -1,8 +1,23 @@
+// Performance degrades very fast on bigger maps:
+// 5x4 ~ 0.23ms
+// 25x21 ~ 32ms
+// 30x30 ~ 926ms
+
+// Idea: Use this only on a subset of the map (size of the viewport)?
+
 import { TileType } from '../constants/tiles';
 import { Position } from '../typings/position';
-import { getSurroundingPositions } from './getSurroundingPositions';
+
+const getAdjacentPositions = (p: Position, mapWidth: number, mapHeight: number): Position[] => {
+  const up: Position = [p[0], Math.max(0, p[1] - 1)];
+  const down: Position = [p[0], Math.min(mapHeight - 1, p[1] + 1)];
+  const left: Position = [Math.max(0, p[0] - 1), p[1]];
+  const right: Position = [Math.min(p[0] + 1, mapWidth - 1), p[1]];
+  return [up, down, left, right];
+};
 
 export const getDijkstraMap = (map: TileType[][], target: Position): string[][] => {
+  // const t0 = performance.now();
   const mapWidth = map[0].length;
   const mapHeight = map.length;
   const result: string[][] = map.map((row) => row.map((cell) => (cell === '#' ? '#' : '')));
@@ -15,16 +30,9 @@ export const getDijkstraMap = (map: TileType[][], target: Position): string[][] 
     const newFrontier: Position[] = [];
     frontier.forEach((position) => {
       result[position[1]][position[0]] = String(getDistance());
-      const surroundingPositions = getSurroundingPositions({
-        // TODO: Use simpler func to get neighboors
-        position,
-        radius: 1,
-        mapWidth,
-        mapHeight,
-      });
-      surroundingPositions.forEach((p) => {
+      const adjacentPositions = getAdjacentPositions(position, mapWidth, mapHeight);
+      adjacentPositions.forEach((p) => {
         if (map[p[1]][p[0]] !== '#' && result[p[1]][p[0]] === '') {
-          // TODO: Exclude origin removes need for this check
           newFrontier.push(p);
         }
       });
@@ -32,5 +40,7 @@ export const getDijkstraMap = (map: TileType[][], target: Position): string[][] 
     distance++;
     frontier = [...newFrontier];
   }
+  // const t1 = performance.now();
+  // console.log('Call to doSomething took ' + (t1 - t0) + ' milliseconds.');
   return result;
 };
