6a35240d397455669054bcb1ab8c4afaceea4d95
TimAstier <timothee.astier@storytel.com>
2021-05-18 07:20:37 +0200

    Refactor reduceMovePlayer

--- src/game-logic/reduceMovePlayer.ts
+++ src/game-logic/reduceMovePlayer.ts
@@ -1,6 +1,6 @@
 import { GRID_HEIGHT, GRID_WIDTH } from '../constants/config';
 import { CreatureType } from '../constants/creatures';
-import { getTile, Tile, TileType } from '../constants/tiles';
+import { getTile, Tile } from '../constants/tiles';
 import { MoveDirection } from '../typings/moveDirection';
 import { Position } from '../typings/position';
 import { checkCreaturesDeath } from './checkCreaturesDeath';
@@ -11,68 +11,14 @@ import { resolveStartingAndEndingConditions } from './resolveStartingAndEndingCo
 import { updateBurningTiles } from './updateBurningTiles';
 import { updateVisibility } from './updateVisibility';
 
-export const reduceMovePlayer = (draft: GameState, moveDirection: MoveDirection): void => {
+const getNextPosition = (draft: GameState, moveDirection: MoveDirection): Position => {
   let nextTileX: number;
   let nextTileY: number;
-  let nextTileType: TileType;
-  let nextTile: Tile | undefined;
-  let creature;
-
-  if (draft.currentMap === null) {
-    return;
-  }
-
-  draft.interactionText = '';
-  draft.moveDirection = moveDirection;
-
-  // Resolve playerConditions
-  resolveConditions(draft);
-
-  const moveToNewPosition = (position: Position) => {
-    // Empty previous location
-    draft.currentMap[draft.playerPosition[1]][draft.playerPosition[0]].content = 0;
-
-    // Loot item
-    lootItem(draft, position);
-
-    // Move player
-    draft.currentMap[position[1]][position[0]].content = 'Player';
-
-    // Update player position
-    draft.playerPosition = position;
-
-    // Update visibility
-    draft.currentMap = updateVisibility(position, draft.currentMap);
-  };
-
-  const moveAndStayAtSamePosition = (tileNameInSentence: string | undefined) => {
-    draft.interactionText = `You hit ${tileNameInSentence || ''}.`;
-  };
-
-  const attackCreature = (id: string, type: CreatureType) => {
-    draft.creatures[id].hp = draft.creatures[id].hp - 5;
-    draft.eventLogs.push(`You hit the ${type} for 5 damage!`);
-  };
-
-  //  TODO: DRY this
   switch (moveDirection) {
     case 'Left':
       nextTileX =
         draft.playerPosition[0] > 0 ? draft.playerPosition[0] - 1 : draft.playerPosition[0];
       nextTileY = draft.playerPosition[1];
-      nextTileType = draft.currentMap[nextTileY][nextTileX].tile;
-      nextTile = getTile(nextTileType);
-
-      creature = draft.currentMap[nextTileY][nextTileX].creature;
-      if (creature) {
-        attackCreature(creature.id, creature.type);
-        break;
-      }
-      if (draft.playerPosition[0] > 0 && nextTile?.canWalkThrough === true) {
-        moveToNewPosition([nextTileX, nextTileY]);
-        break;
-      }
-      moveAndStayAtSamePosition(nextTile?.nameInSentence);
       break;
     case 'Right':
       nextTileX =
@@ -80,68 +26,76 @@ export const reduceMovePlayer = (draft: GameState, moveDirection: MoveDirection)
           ? draft.playerPosition[0] + 1
           : draft.playerPosition[0];
       nextTileY = draft.playerPosition[1];
-      nextTileType = draft.currentMap[nextTileY][nextTileX].tile;
-      nextTile = getTile(nextTileType);
-
-      creature = draft.currentMap[nextTileY][nextTileX].creature;
-      if (creature) {
-        attackCreature(creature.id, creature.type);
-        break;
-      }
-      if (draft.playerPosition[0] < GRID_WIDTH - 1 && nextTile?.canWalkThrough === true) {
-        moveToNewPosition([nextTileX, nextTileY]);
-        break;
-      }
-      moveAndStayAtSamePosition(nextTile?.nameInSentence);
       break;
-
     case 'Up':
       nextTileX = draft.playerPosition[0];
       nextTileY =
         draft.playerPosition[1] > 0 ? draft.playerPosition[1] - 1 : draft.playerPosition[1];
-      nextTileType = draft.currentMap[nextTileY][nextTileX].tile;
-      nextTile = getTile(nextTileType);
-
-      creature = draft.currentMap[nextTileY][nextTileX].creature;
-      if (creature) {
-        attackCreature(creature.id, creature.type);
-        break;
-      }
-      if (draft.playerPosition[1] > 0 && nextTile?.canWalkThrough === true) {
-        moveToNewPosition([nextTileX, nextTileY]);
-        break;
-      }
-      moveAndStayAtSamePosition(nextTile?.nameInSentence);
       break;
-
     case 'Down':
       nextTileX = draft.playerPosition[0];
       nextTileY =
         draft.playerPosition[1] < GRID_HEIGHT - 1
           ? draft.playerPosition[1] + 1
           : draft.playerPosition[1];
-      nextTileType = draft.currentMap[nextTileY][nextTileX].tile;
-      nextTile = getTile(nextTileType);
+  }
+  return [nextTileX, nextTileY];
+};
 
-      creature = draft.currentMap[nextTileY][nextTileX].creature;
-      if (creature) {
-        attackCreature(creature.id, creature.type);
-        break;
-      }
-      if (draft.playerPosition[1] < GRID_HEIGHT - 1 && nextTile?.canWalkThrough === true) {
-        moveToNewPosition([nextTileX, nextTileY]);
-        break;
-      }
-      moveAndStayAtSamePosition(nextTile?.nameInSentence);
-      break;
+const getCanWalkToNextPosition = (
+  draft: GameState,
+  moveDirection: MoveDirection,
+  nextTile: Tile | undefined
+): boolean => {
+  switch (moveDirection) {
+    case 'Left':
+      return draft.playerPosition[0] > 0 && nextTile?.canWalkThrough === true;
+    case 'Right':
+      return draft.playerPosition[0] < GRID_WIDTH - 1 && nextTile?.canWalkThrough === true;
+    case 'Up':
+      return draft.playerPosition[1] > 0 && nextTile?.canWalkThrough === true;
+    case 'Down':
+      return draft.playerPosition[1] < GRID_HEIGHT - 1 && nextTile?.canWalkThrough === true;
+  }
+};
+
+const moveToNewPosition = (draft: GameState, position: Position) => {
+  draft.currentMap[draft.playerPosition[1]][draft.playerPosition[0]].content = 0;
+  lootItem(draft, position);
+  draft.currentMap[position[1]][position[0]].content = 'Player';
+  draft.playerPosition = position;
+  draft.currentMap = updateVisibility(position, draft.currentMap);
+};
+
+const moveAndStayAtSamePosition = (draft: GameState, tileNameInSentence: string | undefined) => {
+  draft.interactionText = `You hit ${tileNameInSentence || ''}.`;
+};
+
+const attackCreature = (draft: GameState, id: string, type: CreatureType) => {
+  draft.creatures[id].hp = draft.creatures[id].hp - 5;
+  draft.eventLogs.push(`You hit the ${type} for 5 damage!`);
+};
+
+export const reduceMovePlayer = (draft: GameState, moveDirection: MoveDirection): void => {
+  draft.interactionText = '';
+  draft.moveDirection = moveDirection;
+
+  resolveConditions(draft);
+
+  const nextPosition = getNextPosition(draft, moveDirection);
+  const nextTileType = draft.currentMap[nextPosition[1]][nextPosition[0]].tile;
+  const nextTile = getTile(nextTileType);
+  const creature = draft.currentMap[nextPosition[1]][nextPosition[0]].creature;
+
+  if (creature) {
+    attackCreature(draft, creature.id, creature.type);
+  } else if (getCanWalkToNextPosition(draft, moveDirection, nextTile)) {
+    moveToNewPosition(draft, nextPosition);
+  } else {
+    moveAndStayAtSamePosition(draft, nextTile?.nameInSentence);
   }
 
-  // Update burning tiles
   draft.currentMap = updateBurningTiles(draft.currentMap);
-
-  // Resolve starting and ending conditions
   resolveStartingAndEndingConditions(draft);
-
-  // Check for dead creatures
   checkCreaturesDeath(draft);
 };
